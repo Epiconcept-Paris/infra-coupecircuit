b233ad5ea50cf77d6f2efc471de3a48a404c34ac
Cédric Girard <cedric@daneel.net>
2020-03-30 17:08:17 +0200

    poc

--- poc_code_applicatif.php
+++ poc_code_applicatif.php
@@ -2,18 +2,23 @@
 #simulation, à mettre dans un script autre
 apc_store('NB_SESSIONS_PHP', 210);
 apc_store('COUPURE_CHARGE_SERVEUR', FALSE);
+apc_store('LOAD_AVG', array(1, 1.2, 0.5));
 
 #code proposé par l'infra
-$bCoupureCharge = apc_fetch('COUPURE_CHARGE_SERVEUR');
 #si COUPURE_CHARGE_SERVEUR n'est pas défini, $bCoupureCharge vaudra FALSE
+$iNbSessionsPHPMax = intval(getenv('NBMAX_SESSIONS_PHP')) ?: 300;
+$maxLoadAvg5 = intval(getenv('MAX_LOAD_AVG_5')) ?: 8;
+$maxLoadAvg10 = intval(getenv('MAX_LOAD_AVG_10')) ?: 8;
 
-$iNbSessionsPHPMax = = intval(getenv('NBMAX_SESSIONS_PHP')) ?: 100;
-$loadAvgTTL = intval(getenv('LOAD_AVG_TTL')) ?: 1;
-$maxLoadAvg = intval(getenv('MAX_LOAD_AVG')) ?: 6;
-
+$bCoupureCharge = boolval(apc_fetch('COUPURE_CHARGE_SERVEUR'));
 $aLoadAvg = apc_fetch('LOAD_AVG');
+$iNbSessionsPHP = apc_fetch('NB_SESSIONS_PHP');
+if($iNbSessionsPHP === FALSE) {
+	$iNbSessionsPHP = 0;
+	error_log('pas de donnée NB_SESSIONS_PHP disponible');
+}
 
-if($iNbSessionsPHP > $iNbSessionsPHPMax || $bCoupureCharge === TRUE || ($aLoadAvg[0] > $maxLoadAvg && $aLoadAvg[1] > $maxLoadAvg)) {
+if($iNbSessionsPHP > $iNbSessionsPHPMax || $bCoupureCharge === TRUE || ($aLoadAvg[0] > $maxLoadAvg5 && $aLoadAvg[1] > $maxLoadAvg10)) {
 	error_log(sprintf('connection_refused,ip:%s,SESSIONS:%d/%d,LOADAVG:%0.2f/%0.2f/%0.2f,COUPURE:%s', 
 		$_SERVER['REMOTE_ADDR'], 
 		$iNbSessionsPHP, $iNbSessionsPHPMax, 
